ba15a47f8c7ecc91ac999b49c753c5acbd6d514b
Damon A. Heath <daheath2007@gmail.com>
2025-05-21 09:24:27 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,11 @@ APP_NAME="twiinz-beard-website"
 echo "Logging into Heroku container registry..."
 heroku container:login
 
-echo "Building new Docker image..."
-docker build --no-cache -t main:latest .
+echo "Setting Heroku stack to container..."
+heroku stack:set container -a $APP_NAME
+
+echo "Building new Docker image (linux/amd64)..."
+docker build --platform=linux/amd64 --no-cache -t main:latest .
 
 echo "Tagging Docker image..."
 docker tag main:latest registry.heroku.com/$APP_NAME/web
@@ -16,6 +19,15 @@ docker push registry.heroku.com/$APP_NAME/web
 
 echo "Releasing Docker container..."
 heroku container:release web -a $APP_NAME
+
+echo "Checking if web dyno is running..."
+DYNO_COUNT=$(heroku ps -a $APP_NAME | grep web | wc -l)
+if [ "$DYNO_COUNT" -eq "0" ]; then
+  echo "No web dyno running. Scaling web dyno to 1..."
+  heroku ps:scale web=1 -a $APP_NAME
+else
+  echo "Web dyno already running."
+fi
 
 echo "Checking process status..."
 heroku ps -a $APP_NAME
